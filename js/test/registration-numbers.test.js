95a55914226c570f34d9e2656ad13dca93e64019
Thembisile <shaundamon6@icloud.com>
2018-05-22 11:18:46 +0200

    tests

--- js/test/registration-numbers.test.js
+++ js/test/registration-numbers.test.js
@@ -2,33 +2,33 @@ describe('Test Registration Numbers Widget', function(){
   it('should return true if registration matches given prefix for Cape Town', function(){
     var callReg = RegistrationFactory();
 
-    assert.equal(callReg.addReg('CA 9870'), true);
+    assert.equal(callReg.additionReg('CA 9870'), true);
   })
   it('should return true if town matches given prefix for Bellville', function(){
     var callReg = RegistrationFactory();
 
-    assert.equal(callReg.addReg('CY 1254'), true);
+    assert.equal(callReg.additionReg('CY 1254'), true);
   })
   it('should return true if town matches given prefix for Plettenrburg', function(){
     var callReg = RegistrationFactory();
 
-    assert.equal(callReg.addReg('CX 0125'), true)
+    assert.equal(callReg.additionReg('CX 0125'), true)
   })
   it('should return true if town matches given prefix for George', function(){
     var callReg = RegistrationFactory();
 
-    assert.equal(callReg.addReg('CAW 8745'), true)
+    assert.equal(callReg.additionReg('CAW 8745'), true)
   });
   it('should return true if town matches given prefix for Paarl', function(){
     var callReg = RegistrationFactory();
 
-    assert.equal(callReg.addReg('CJ 8457'), true);
+    assert.equal(callReg.additionReg('CJ 8457'), true);
   })
   it('should return false if registration is from another town and not from selected towns', function(){
     var callReg = RegistrationFactory();
 
-    callReg.addReg('CZ 48636');
+    callReg.additionReg('CZ 48636');
 
-    assert.equal(false, callReg.getReg());
+    assert.equal(false, callReg.getRegistry());
   })
 });
